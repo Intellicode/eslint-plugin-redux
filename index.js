f6ba3cb76aca7982cc2394d0a92636fe43b04019
Tom Hastjarjanto <tom@intellicode.nl>
2016-06-22 20:58:19 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  rules: {
+  },
+  rulesConfig: {
+  }
+};
---
+++ tests/index.js
@@ -0,0 +1,43 @@
+/* eslint-env mocha */
+'use strict';
+
+const plugin = require('..');
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const rules = fs.readdirSync(path.resolve(__dirname, '../lib/rules/'))
+  .map((f) => path.basename(f, '.js'));
+
+const defaultSettings = {
+  'jsx-uses-vars': 1,
+};
+
+describe('all rule files should be exported by the plugin', () => {
+  rules.forEach((ruleName) => {
+    it('should export ' + ruleName, () => {
+      assert.equal(
+        plugin.rules[ruleName],
+        require(path.join('../lib/rules', ruleName))
+      );
+    });
+
+    if (defaultSettings.hasOwnProperty(ruleName)) {
+      const val = defaultSettings[ruleName];
+      it('should configure ' + ruleName + ' to ' + val + ' by default', () => {
+        assert.equal(
+          plugin.rulesConfig[ruleName],
+          val
+        );
+      });
+    } else {
+      it('should configure ' + ruleName + ' off by default', () => {
+        assert.equal(
+          plugin.rulesConfig[ruleName],
+          0
+        );
+      });
+    }
+  });
+});
